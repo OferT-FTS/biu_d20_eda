96ff13a0771b84c85752ab2c64b520c6110cc2fb
OferT-FTS <ofer@il-fts.com>
2025-08-06 00:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -396,7 +396,7 @@ else:
                         # Seaborn pairplot
                         @st.cache_data
                         def generate_pairplot(data, hue):
-                            return sns.pairplot(data, hue=hue, diag_kind="hist", size=3)
+                            return sns.pairplot(data, hue=hue, diag_kind="hist", size=2)
 
                         fig = generate_pairplot(df, hue_col)
                         st.pyplot(fig)
