4fe3e3a4c46f90de9f793deeb5054bef7caa1769
OferT-FTS <ofer@il-fts.com>
2025-08-15 16:15:23 +0300

    added nunique table

--- main.py
+++ main.py
@@ -168,11 +168,13 @@ else:
                     # convert column types
                     col1, col2 = st.columns([2, 3])
                     with col1:
+                        st.write(df.nunique())
+
+                    with col2:
                         st.markdown("###### Select a column to change its type ")
                         change_type_col = st.selectbox("Select column for type conversion:", df.columns)
                         new_type = st.selectbox("Change to a new data type:", ["string", "numeric", "datetime"])
-                    with col2:
-                        st.empty()
+                        
                     if st.button("Change Type"):
                         if new_type == "string":
                             df[change_type_col] = df[change_type_col].astype(str)
