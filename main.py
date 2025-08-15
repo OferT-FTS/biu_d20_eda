421b8cd3e52d4799bef1093841bfa036fa14213d
OferT-FTS <ofer@il-fts.com>
2025-08-15 18:24:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -267,10 +267,11 @@ else:
                                 with col3:
                                     st.write("###### Missing Values Table")
                                     st.dataframe(missing_df.reset_index(drop=True))
-                                if st.button("Push Button To Delete All Missing Values Records...", key="delete_missing"):
-                                    st.write(" Missing Values Records Deleted!")
-                                    st.session_state.df = st.session_state.df.dropna()
-                                    st.rerun()
+                                if missing_df.empty:
+                                    if st.button("Push Button To Delete All Missing Values Records...", key="delete_missing"):
+                                        st.write(" Missing Values Records Deleted!")
+                                        st.session_state.df = st.session_state.df.dropna()
+                                        st.rerun()
                             else:
                                 st.success("No missing values detected in the dataset!")
                     else:
